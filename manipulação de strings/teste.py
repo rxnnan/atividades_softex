f8cf3d4bdc12a493bd1b815070598196e6fb8f80
rxnnan <107962493+rxnnan@users.noreply.github.com>
2022-07-25 17:18:11 -0300

    Add files via upload

---
+++ manipulação de strings/teste.py
@@ -0,0 +1,20 @@
+def contaLetra(palavra, caractere):
+    aux = palavra.count(caractere)
+    print(f"Na string {palavra} temos {aux} caracteres {caractere}.")
+    return aux
+
+def maiusculo(palavra):
+    aux = palavra.upper()
+    print(f"Colocando todas as letras da string {palavra} em maiúscolo vamos ter: {aux}")
+    return aux
+
+def tamanhoString(palavra):
+    aux = len(palavra)
+    print(f"A string {palavra} tem {aux} caracteres.")
+    return aux         
+
+p = str(input("Informe uma string: "))
+c = str(input(f"Qual caractere você que contar na palavra {p}? "))
+tamanhoString(p)
+contaLetra(p, c)
+maiusculo(p)
