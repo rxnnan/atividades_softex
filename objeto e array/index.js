95007779445409fa37b6541f29dbe115106794f6
Rennan Carbas <107962493+rxnnan@users.noreply.github.com>
2022-09-16 21:20:07 -0300

    Add files via upload

---
+++ objeto e array/index.js
@@ -0,0 +1,25 @@
+let array = ['Banana', 'Palavra', 'Sorvete', 'Abacaxi', 'Carro']
+
+let Banco = {
+    conta: 235711,
+    saldo: 2000,
+    tipoDeConta: 'Corrente',
+    agencia: '013',
+}
+
+function listaPropriedades(obj) {
+    console.log('As propriedades do objeto são: ')
+    for (let propriedade in obj) {
+        console.log(`${propriedade}: ${obj[propriedade]}`)
+    }
+}
+
+function listaElementos(arr) {
+    console.log('Os elementos do array são: ')
+    for (let elemento of arr) {
+        console.log(`${elemento}`)
+    }
+}
+
+listaPropriedades(Banco)
+listaElementos(array)
