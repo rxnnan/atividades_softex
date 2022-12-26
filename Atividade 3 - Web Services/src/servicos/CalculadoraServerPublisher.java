5982c0fd0bf8386c35e12ea33d0e095eabea726d
Rennan Carbas <107962493+rxnnan@users.noreply.github.com>
2022-12-26 18:31:57 -0300

    Add files via upload

---
+++ Atividade 3 - Web Services/src/servicos/CalculadoraServerPublisher.java
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author RENNAN
+ */
+package servicos;
+
+import javax.xml.ws.Endpoint;
+
+public class CalculadoraServerPublisher {
+
+  public static void main(String[] args)
+  {
+    Endpoint.publish("http://127.0.0.1:8080/calc",
+    new CalculadoraServerImpl());
+  }
+}
